8b3bf356ee41691711a1da6d16b6111dcb564e19
Chaouki Dhib <chaodhib@gmail.com>
2015-05-19 06:30:58 +0200

    fixed a bug where my 2 addons would share the isActive variable (meaning that either both were active or both were disabled) which was not intended.

--- InterruptInRaid/InterruptInRaid.lua
+++ InterruptInRaid/InterruptInRaid.lua
@@ -14,34 +14,34 @@ local L = MyLocalizationTable;
 -----------------------------------------------------------------------------------
 -- Initializing variables
 -----------------------------------------------------------------------------------
-if (isActive == nil) then isActive = true; end
-if (shortOutput == nil) then shortOutput = false; end
+if (IIR_isActive == nil) then IIR_isActive = true; end
+if (IIR_shortOutput == nil) then IIR_shortOutput = false; end
 -----------------------------------------------------------------------------------
 
 local InterruptInRaid = CreateFrame('Frame')
 
-function UnitIsPlayer(GUID)
+local function UnitIsPlayerByGUID(GUID)
 	local typeFlag=tonumber(GUID:sub(5,5), 16) % 8;
-	return (typeFlag==0)
+	return (typeFlag==0) -- 0=player, 1=world object, 3=NPC, 4 permanent pet, 5 vehicule. 
 end
 
 local function OnEvent(self, event, ...)
 
-	if not isActive then return end -- check if the addon is active
+	if not IIR_isActive then return end -- check if the addon is active
 	if GetNumRaidMembers()==0 then return end -- check if we are in a raid group
 	if select(2,...) ~= "SPELL_INTERRUPT"  then return end -- we only care about spell interrupt events
 	
 	local sourceGUID=select(3,...)
-	if not UnitIsPlayer(sourceGUID) then return end -- check if the source of the interupt is indeed a player
+	if not UnitIsPlayerByGUID(sourceGUID) then return end -- check if the source of the interupt is indeed a player
 	local targetGUID=select(6,...)
-	if UnitIsPlayer(targetGUID) then return end -- check if the target of the interupt is indeed NOT a player
+	if UnitIsPlayerByGUID(targetGUID) then return end -- check if the target of the interupt is indeed NOT a player
 
 	local targetSpellID=select(12,...);
 	local targetName=select(7,...);
 	local sourceSpellID=select(9,...);
 	local sourceName=select(4,...);
 
-	if not shortOutput then
+	if not IIR_shortOutput then
 		SendChatMessage("Interruption OF "..GetSpellLink(targetSpellID).." made ON "..targetName.." WITH "..GetSpellLink(sourceSpellID) .." BY "..sourceName, "RAID")
 	else
 		SendChatMessage(GetSpellLink(targetSpellID) .." interrupted by " ..sourceName, "RAID")
@@ -52,10 +52,10 @@ end
 -- Slash Commands
 SLASH_InterruptInRaid1 = '/iir'; 
 function SlashCmdList.InterruptInRaid(msg, editbox)
-	if (msg == "on") then isActive = true; print("InterruptInraid is now: Active"); return end
-	if (msg == "off") then isActive = false; print("InterruptInraid is now: Inactive"); return end
-	if (msg == "short") then shortOutput = true; print("The output is now set on: Short"); return end
-	if (msg == "normal") then shortOutput = false; print("The output is now set on: Normal"); return end
+	if (msg == "on") then IIR_isActive = true; print("InterruptInraid is now: Active"); return end
+	if (msg == "off") then IIR_isActive = false; print("InterruptInraid is now: Inactive"); return end
+	if (msg == "short") then IIR_shortOutput = true; print("The output is now set on: Short"); return end
+	if (msg == "normal") then IIR_shortOutput = false; print("The output is now set on: Normal"); return end
 
  print("----------------------------------------------------------");
  print("InterruptInRaid commands");
@@ -65,10 +65,10 @@ function SlashCmdList.InterruptInRaid(msg, editbox)
  print("/iir short - use the short output");
  print("/iir normal- use the normal output");
  print("----------------------------------------------------------");
-  if (isActive == true) then print("InterruptInRaid is currently: Active"); end 
-  if (isActive == false) then print("InterruptInRaid is currently: Inactive"); end
-  if (shortOutput == true) then print("and the output is set on: Short"); end
-  if (shortOutput == false) then print("and the output is set on: Normal"); end
+  if (IIR_isActive == true) then print("InterruptInRaid is currently: Active"); end 
+  if (IIR_isActive == false) then print("InterruptInRaid is currently: Inactive"); end
+  if (IIR_shortOutput == true) then print("and the output is set on: Short"); end
+  if (IIR_shortOutput == false) then print("and the output is set on: Normal"); end
  print("----------------------------------------------------------");
 end
 
